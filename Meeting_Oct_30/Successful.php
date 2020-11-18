d6c7f001a6c5717c38265bc674d0238c77392c33
isaacdasilva1 <60912066+isaacdasilva1@users.noreply.github.com>
2020-11-18 00:11:20 -0500

    Create Successful.php

---
+++ Meeting_Oct_30/Successful.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+</head>
+<body>
+<header>Account creation was successful.</header>
+	<p>Click <a href=http://weblab.salemstate.edu/~mytime/Waiting_Time/Log_in.php>here</a> to login.</p>
+</body>
+</html>
